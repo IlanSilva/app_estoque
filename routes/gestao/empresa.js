4f00bfcf9e75c48985cfc6b2085d2db858520a76
Ilan Silva <ilansantos68@hotmail.com>
2021-07-27 22:05:22 -0300

    Adição e configuração de rotas de gestão

--- routes/gestao/empresa.js
+++ routes/gestao/empresa.js
@@ -3,38 +3,53 @@ const Router = express.Router()
 const db = require("../../database/conexao")
 
 // POST
-Router.post("/empresa", async (req, res) => {
+Router.post("/empresa/novo", async (req, res) => {
     const client = await db.connect()
     const val = req.body
     try{
+        if (!val.nomeempresa || typeof val.nomeempresa == undefined){
+            throw new Error("Nome da empresa não informado!")
+        }
+        const verifica_empresa = await client.query("SELECT * FROM GESTAO.EMPRESAS WHERE NOME ILIKE $1", [val.nomeempresa])
+        if (verifica_empresa.rowCount > 0){
+            throw new Error("Já existe uma empresa com este nome!")
+        }
         const txt_cria_empresa = "INSERT INTO GESTAO.EMPRESAS(NOME) VALUES($1) RETURNING *;"
-        const values_cria_empresa = [val.nome.toUpperCase()]
+        const values_cria_empresa = [val.nomeempresa.toUpperCase()]
         const query_cria_empresa = await client.query(txt_cria_empresa, values_cria_empresa)
 
-        res.status(201).json({
-            mensagem: `Empresa ${query_cria_empresa.rows[0].nome} criada com sucesso com o id ${query_cria_empresa.rows[0].codigo}`,
-        })
+        req.flash("msg_sucesso", "Sucesso na criação da empresa!")
+        res.status(201).redirect("/gestao/empresa/novo")
     }catch(err){
-        res.status(404).json({mensagem: "Houve um erro! ", erro: true})
+        req.flash("msg_erro", "Houve um erro! " + err)
+        res.status(404).redirect("/gestao/empresa/novo")
     }finally{
         client.release()
     }
 })
 // GET
 
-Router.get("/empresa", async (req, res) => {
+Router.get("/empresa", (req, res) => {
+    res.status(200).render("pages/empresa/empresa.handlebars")
+})
+
+Router.get("/empresa/novo", (req, res) => {
+    res.status(200).render("pages/empresa/novo")
+})
+
+Router.get("/empresa/visualizar",async (req, res) => {
     const client = await db.connect()
     try{
-        const txt_busca_empresa = "SELECT * FROM GESTAO.EMPRESAS"
-        const query_busca_empresa = await client.query(txt_busca_empresa)
-        res.render("pages/empresa.handlebars")
+        const query_busca_empresas = await client.query("SELECT * FROM GESTAO.EMPRESAS;")
+        console.log(query_busca_empresas.rows)
+        res.status(200).render("pages/empresa/visualizar", {empresas: query_busca_empresas.rows})
     }catch(err){
-        res.status(404).json({mensagem: "Houve um erro! ", erro: true})
+        req.flash("msg_erro", "Houve um erro! " + err)
+        res.status(404).render("pages/empresa/visualizar")
     }finally{
         client.release()
     }
 })
-
 // PUT
 Router.put("/empresa/:id", async (req, res) => {
     const client = await db.connect()
@@ -55,6 +70,7 @@ Router.put("/empresa/:id", async (req, res) => {
         client.release()
     }
 })
+
 
 // DELETE
 
--- routes/gestao/index.js
+++ routes/gestao/index.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const Router = express.Router()
 
+// ROTAS
+
+Router.use("/", (req, res) => {
+    res.render("pages/gestao")
+})
+
 // IMPORTAÇÃO DE MODULOS
 const rotaEmpresa = require("./empresa")
 const rotaEstoque = require("./estoque")
